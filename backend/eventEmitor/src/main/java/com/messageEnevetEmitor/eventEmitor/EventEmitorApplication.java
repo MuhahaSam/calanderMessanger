3005c1dd21e3ae706382d8dbb54e5f0937be26a5
sam <you@example.com>
2025-04-02 15:42:22 +0300

    feat: init backend

---
+++ backend/eventEmitor/src/main/java/com/messageEnevetEmitor/eventEmitor/EventEmitorApplication.java
@@ -0,0 +1,13 @@
+package com.messageEnevetEmitor.eventEmitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEmitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEmitorApplication.class, args);
+	}
+
+}
---
+++ backend/eventEmitor/src/test/java/com/messageEnevetEmitor/eventEmitor/EventEmitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messageEnevetEmitor.eventEmitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEmitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
